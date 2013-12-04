e318ea7df1a7f69cd91a72485c79714b871d81ef
hgarcia <hernan@dynamicprogrammer.com>
2013-12-03 23:32:18 -0500

    [HG] Add configurable tokens (not everything will work).

--- src/substitute.js
+++ src/substitute.js
@@ -1,17 +1,24 @@
-function replacer(str, key, value) {
-  var re = new RegExp("{{" + key + "}}", "g");
+function replacer(str, key, value, st, et) {
+  var re = new RegExp(st + key + et, "g");
   return str.replace(re, value);
 }
 
-exports.render = function(str, values) {
-  if (Array.isArray(values)) {
-    values.forEach(function (val, index) {
-      str = replacer(str, "[" + index + "]", val);
-    });
-  } else {
-    Object.keys(values).forEach(function (key) {
-      str = replacer(str, key, values[key]);
-    });
-  }
-  return str;
+exports.create = function (options) {
+  var st = options && options.startToken || "{{";
+  var et = options && options.endToken || "}}";
+
+  return {
+    render: function(str, values) {
+      if (Array.isArray(values)) {
+        values.forEach(function (val, index) {
+          str = replacer(str, "[" + index + "]", val, st, et);
+        });
+      } else {
+        Object.keys(values).forEach(function (key) {
+          str = replacer(str, key, values[key], st, et);
+        });
+      }
+      return str;
+    }
+  };
 };
--- test/substitute.test.js
+++ test/substitute.test.js
@@ -3,36 +3,56 @@ var substitute = require('../index');
 
 describe('substitute', function() {
 
+  describe('.create', function () {
+
+    it('should create a new substitute', function () {
+      var s = substitute.create();
+      s.should.have.property('render');
+    });
+
+    it('should change the token delimiters', function () {
+      var s = substitute.create({startToken: "<%", endToken: "%>"});
+      var result = s.render("With <%0%>", ["other tokens"]);
+      result.should.equal("With other tokens");
+    });
+  });
+
   describe('.render("str", {value: })', function () {
+
+    var s = substitute.create();
+
     it('should interpolate the value', function () {
-      var result = substitute.render("String with {{token}}", {token: "a token"});
+      var result = s.render("String with {{token}}", {token: "a token"});
       result.should.equal("String with a token");
     });
 
     it('should replace all occurrences', function () {
-      var result = substitute.render("String {{token}} with {{token}}", {token: "a token"});
+      var result = s.render("String {{token}} with {{token}}", {token: "a token"});
       result.should.equal("String a token with a token");
     });
 
     it("should replace more than one value", function () {
-      var result = substitute.render("{{obj}} with {{token}}", {token: "a token", obj: "The string"});
+      var result = s.render("{{obj}} with {{token}}", {token: "a token", obj: "The string"});
       result.should.equal("The string with a token");
     });
   });
 
   describe('.render("str", [values])', function () {
+
+    var s = substitute.create();
+
     it('should interpolate values per index', function () {
-      var result = substitute.render("String with {{0}}", ["a token"]);
+      var result = s.render("String with {{0}}", ["a token"]);
       result.should.equal("String with a token");
     });
 
     it('should replace all occurrences', function () {
-      var result = substitute.render("String {{0}} with {{0}}", ["a token"]);
+      var result = s.render("String {{0}} with {{0}}", ["a token"]);
       result.should.equal("String a token with a token");
     });
 
     it("should replace more than one value", function () {
-      var result = substitute.render("{{1}} with {{0}} in {{1}}", ["a token", "The string"]);
+      var result = s.render("{{1}} with {{0}} in {{1}}", ["a token", "The string"]);
       result.should.equal("The string with a token in The string");
     });
   });
